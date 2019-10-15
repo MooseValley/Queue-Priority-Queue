805fb54a13c53fdaa860345ab3b4c7170ebf5c6d
MooseValley <Moose_Software@yahoo.com.au>
2019-10-15 16:42:12 +1000

    Initial Commit

---
+++ PriorityQueueTestHigh.java
@@ -0,0 +1,105 @@
+/*
+ Author: Mike O'Malley
+ Source: PriorityQueueTest.java
+Descrtn: Priority Queue of Tasks where HIGH priority values = HIGH priority.
+
+Ammendment History
+Ver   Date        Author    Details
+----- ----------- --------  ---------------------------------------------------
+0.001 15-Oct-2019 Mike O    Created.
+
+*/
+
+import java.util.*;
+
+class Task implements Comparable
+{
+   private int    priority;
+   private String description;
+
+   public Task()
+   {
+      priority = 0;
+      description = "";
+   }
+
+   public Task (int priority, String description)
+   {
+      this.priority    = priority;
+      this.description = description;
+   }
+
+   public int getPriority ()
+   {
+      return priority;
+   }
+
+   public String getDescription ()
+   {
+      return description;
+   }
+
+   public void setPriority (int priority)
+   {
+     this.priority    = priority;
+   }
+
+   public void setDescription (String description)
+   {
+      this.description = description;
+   }
+
+   @Override
+   public int compareTo (Object obj)
+   {
+      int result = -1; // Not the same.
+
+      if (obj == null)
+      {
+          result = -1; // Not the same.
+      }
+
+      else if (this == obj)
+      {
+         result = 0; // Memory addresses are the same, so we are comparing an object
+                     // to itself.
+      }
+
+      else if (obj instanceof Task)
+      {
+         Task objTask = (Task) obj;
+
+         if (getPriority() > objTask.getPriority())
+            return -1;  // The prioroty for this < obj
+         else if (getPriority() == objTask.getPriority())
+            return 0;  // The prioroty for this == obj
+         else
+            return 1;  // The prioroty for this > obj
+      }
+
+      return result;
+   }
+
+   @Override
+   public String toString()
+   {
+      return priority + "\t" + description;
+   }
+}
+
+public class PriorityQueueTestHigh
+{
+   public static void main (String[] args)
+   {
+      PriorityQueue<Task> tasks = new PriorityQueue<Task>();
+
+      tasks.add (new Task (10, "Buy a Quadbike") );
+      tasks.add (new Task (9, "Buy health insurance") );
+      tasks.add (new Task (5, "Learn tricks on Quadbike") );
+      tasks.add (new Task (1, "Jump Quadbike over cars") );
+
+      for (Task task : tasks)
+         System.out.println (task.toString() );
+
+   }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Queue-Priority-Queue
-Queue - Priority Queue
+
+Explore Java's built-in **PriorityQueue** class.
+
+Create a **Task** list for tasks with various priorities and add them to a PriorityQueue and iterate through them and display them to the screen in order from highest priority to lowest priority.
+
+Also switch how the priority works:
+
+* Initially low numbers = high priority.
+
+* However, I show a simple way to reverse this and make high numbers = high priority.
+
+
+## TODO: for students:
+
+* Add more Tasks, jumble up the order in which they are added, and confirm for yourself that the items are always retrieved in priority order.
+
+* Explore alternate ways to iterate through / access the PriorityQueue, such as:
+```
+   while (!pq.isEmpty())
+      System.out.println(pq.remove());
+```
+
+* Now you do it: create a **Patient** class and add Patient objects to a PriorityQueue and simulate a hospital waiting room.
+
